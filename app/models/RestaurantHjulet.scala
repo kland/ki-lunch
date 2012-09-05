c2af658698852f6101b303ec3e961d11a852f6ba
Karl Landström <karl.landstrom@ki.se>
2012-09-05 10:30:22 +0200

    Added sentinel "veckans tips" for friday's dishes at Hjulet

--- app/models/RestaurantHjulet.scala
+++ app/models/RestaurantHjulet.scala
@@ -10,9 +10,13 @@ class RestaurantHjulet(override val name: String, override val url: String) exte
 
 		//extract the content between the third occurrence of `weekday' and the third occurence of `nextWeekday'
 		val pos1 = (("""(?i)\b""" + weekday + """\b""").r findAllIn document).matchData.toSeq(2).end
-		val pos2 = (("""(?i)\b""" + nextWeekday + """\b""").r findAllIn document).matchData.toSeq(2).start
+		val pos2 = 
+			if (weekday != "friday")
+				(("""(?i)\b""" + nextWeekday + """\b""").r findAllIn document).matchData.toSeq(2).start
+			else 
+				(("""(?i)\bveckans tips\b""").r findAllIn document).matchData.toSeq(0).start
 		var documentPart = document.substring(pos1, pos2)
-				
+
 		//replace each newline with a space
 		documentPart = documentPart.replace('\n', ' ')
 		
@@ -27,10 +31,5 @@ class RestaurantHjulet(override val name: String, override val url: String) exte
 		
 		//split at linebreaks into a list of dishes
 		documentPart.split("\n").toList
-		
-		//val matches = ("""(?i)\bfredag\b""".r findAllIn document).matchData map (_.end)
-		//val matches = """(?i)\btisdag\b""".r findAllIn document
-		//println(matches.matchData.toSeq.last)
-		//List("soppa", "knödel")
 	}
 }
