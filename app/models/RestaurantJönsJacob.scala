5c5e5e30483430dcecd40b8614283b8fe5086e1f
Karl Landström <karl.landstrom@ki.se>
2012-09-05 11:34:47 +0200

    Implemented RestaurantJönsJacob.parsedDishes

---
+++ app/models/RestaurantJönsJacob.scala
@@ -0,0 +1,43 @@
+package models
+
+import lib.Util
+
+class RestaurantJönsJacob(override val name: String, override val url: String) extends Restaurant(name, url) {
+
+	def parsedDishes(document: String, weekday: String) = {
+		val nextWeekday = Util.nextWeekday(weekday)
+
+		//extract the content between the third occurrence of `weekday' and the third occurence of `nextWeekday'
+		val pos1 = (("""(?i)\b""" + weekday + """\b(\w|\s)*""").r findAllIn document).matchData.toSeq(0).end
+		val pos2 = 
+			if (weekday != "friday")
+				(("""(?i)\b""" + nextWeekday + """\b""").r findAllIn document).matchData.toSeq(0).start
+			else 
+				(("""(?i)<a .*?'Boka bord på din iPhone'""").r findAllIn document).matchData.toSeq(0).start
+		var documentPart = document.substring(pos1, pos2)
+
+		//replace each newline with a space
+		documentPart = documentPart.replace('\n', ' ')
+		
+		//replace each <br> with a newline
+		documentPart = documentPart.replaceAll("(?i)<br>", "\n")
+
+		//remove all remaining tags
+		documentPart = documentPart.replaceAll("</?[^>]*>", "")
+
+		//remove prices
+		documentPart = documentPart.replaceAll("""\d+:-""", "")
+		
+		//remove "dagens" strings
+		documentPart = documentPart.replaceAll("""(?i)dagens (rätt|nyckelhål)""", "")
+
+		//remove multiple newlines
+		documentPart = documentPart.replaceAll("""\n\s*""", "\n")
+
+		//remove leading and trailing whitespace
+		documentPart = documentPart.trim
+		
+		//split at linebreaks into a list of dishes
+		documentPart.split("\n").toList
+	}
+}
--- app/models/Restaurants.scala
+++ app/models/Restaurants.scala
@@ -1,11 +1,12 @@
 package models
 
-import models.{RestaurantHjulet, RestaurantMF}
+import models.{RestaurantHjulet, RestaurantJönsJacob, RestaurantMF}
 
 object Restaurants {
 
 	def all = List(
-		new RestaurantHjulet("Hjulet", "http://web.comhem.se/hjulet/matsedel2.html"), 
+		new RestaurantHjulet("Hjulet", "http://web.comhem.se/hjulet/matsedel2.html"),
+		new RestaurantJönsJacob("Jöns Jacob", "http://gastrogate.com/restaurang/jonsjacob/page/3"),  
 		new RestaurantMF("MF", "http://www.mmcatering.nu/index.php?sajt=mf")
 	)
 
